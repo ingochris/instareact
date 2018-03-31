8e7add65c3fe53f6d2b4dc1446a84dbf7f1cf3d2
Bhargava Ramisetty <bhargava.ramisetty@gmail.com>
2018-03-31 14:56:45 -0700

    Updated photos

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -19,7 +19,7 @@ class MainScreen extends Component {
 
     static navigationOptions = {
         headerLeft: <Icon name="ios-camera-outline" style={{ paddingLeft: 10 }} />,
-        title: "InstaReacc",
+        title: "InstaReact",
         headerRight: <Icon style={{ paddingRight: 10 }} name="ios-send-outline" />
     }
 
