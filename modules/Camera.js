de261b41f798c49b4ee468b61701f60b83b07a28
Sergei Chestakov <schestakov@ucdavis.edu>
2018-03-31 03:04:16 -0700

    Move camera stream to seperate component

---
+++ modules/Camera.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+
+export default class CameraStream extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.front,
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
