2ab4098d9273543876419fdd0f432f36bd981238
Tyler Yep <tyleryep@s-169-232-86-25.resnet.ucla.edu>
2018-03-31 11:38:57 -0700

    Autoscrolling

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -3,13 +3,16 @@ import {
     View,
     Text,
     StyleSheet,
-    ScrollView
+    ScrollView,
+    Dimensions
 } from "react-native";
 
 import { Container, Content, Icon } from 'native-base'
 import CardComponent from '../CardComponent'
+const { width, height } = Dimensions.get('window');
 
 class HomeTab extends Component {
+
 
     static navigationOptions = {
 
@@ -23,7 +26,36 @@ class HomeTab extends Component {
             currentPosition: 0,
             images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         };
-        // this.scrolling = this.scrolling.bind(this);
+        this.scrolling = this.scrolling.bind(this);
+    }
+
+    componentDidMount(){
+      this.activeInterval = setInterval(this.scrolling, 4000);
+    }
+
+    componentWillUnmount(){
+      clearInterval(this.activeInterval);
+    }
+
+    // Scrolling Animation
+    scrolling() {
+      // Start scrolling if there's more than one stock to display
+      if (this.state.images.length > 1) {
+          // Increment position with each new interval
+          position = this.state.currentPosition + height * 0.74;
+          this.ticker.scrollTo({ y: position, animated: true });
+          // After position passes this value, snaps back to beginning
+          let maxOffset = 10000;
+
+          // Set animation to repeat at end of scroll
+          if (this.state.currentPosition > maxOffset) {
+               this.ticker.scrollTo({ y: 0, animated: false })
+               this.setState({ currentPosition: 0 });
+          }
+          else {
+              this.setState({ currentPosition: position });
+          }
+      }
     }
 
     render() {
@@ -31,6 +63,7 @@ class HomeTab extends Component {
             <ScrollView
                 style={styles.scrollview}
                 vertical={true}
+                ref={(ref) => this.ticker = ref}
                 bounces={true}>
                 <Content>
                   {this.state.images.map((item, index) => (
--- Components/CardComponent.js
+++ Components/CardComponent.js
@@ -67,8 +67,7 @@ class CardComponent extends Component {
                 <CardItem>
                     <Body>
                         <Text>
-                            <Text style={{ fontWeight: "900" }}>varun
-                            </Text>
+                            <Text style={{ fontWeight: "900" }}>Tyler </Text>
                             Ea do Lorem occaecat laborum do. Minim ullamco ipsum minim eiusmod dolore cupidatat magna exercitation amet proident qui. Est do irure magna dolor adipisicing do quis labore excepteur. Commodo veniam dolore cupidatat nulla consectetur do nostrud ea cupidatat ullamco labore. Consequat ullamco nulla ullamco minim.
                         </Text>
                     </Body>
